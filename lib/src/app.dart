19817b459a9721bdae2724b34ba47d4fbb3d37dc
Vishal Sharma <vishalsharma.vs@hotmail.com>
2020-10-04 03:16:20 +0530

    stateless to statefull refractoring

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,8 +2,20 @@
 import 'package:flutter/material.dart';
 
 // create a class that will be custom widget
-// this class must extent the 'StatelssWidget' base class
-class App extends StatelessWidget {
+// this class must extent the 'StatefullWidget' base class
+
+// widget class
+class App extends StatefulWidget {
+  // returns an instance of Widget State
+  createState() {
+    return AppState();
+  }
+}
+
+// widget's state class
+class AppState extends State<App> {
+  int counter = 0;
+
   // must define a 'build' method that returns
   // the widgets that *this* widget will show
   Widget build(context) {
@@ -12,9 +24,13 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: Text('Lets see some images!'),
         ),
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            print('Hi there!');
+            // req a func
+            setState(() {
+              counter += 1;
+            });
           },
           child: Icon(Icons.add),
         ),
